5badaac7409a21366c491895dee94ea9d42ea6d6
Nutthanich Narphromar <monaprom134@gmail.com>
2021-03-25 10:29:45 +0700

    Add random id to model classes

--- src/main/java/ku/message/model/Message.java
+++ src/main/java/ku/message/model/Message.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
@@ -13,8 +14,8 @@ import javax.persistence.Id;
 public class Message {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @GeneratedValue
+    private UUID id;
 
     private String user;
     private String text;
